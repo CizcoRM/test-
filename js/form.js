3c453d4196667dffda2177de1cde9eea55f1347c
CizcoRM <62819589+CizcoRM@users.noreply.github.com>
2020-04-03 01:50:13 -0600

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,56 +1,39 @@
 document.querySelector("#submit").addEventListener("click", e => {
-  e.preventDefault();
-
-  //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
-  let telefono = "50370451939";
-
-  let nombre = document.querySelector("#nombre").value;
-  let dui = document.querySelector("#dui").value;
-  let direccion = document.querySelector("#direccion").value;
-  let telefonoc = document.querySelector("#telefonoc").value;
-  let miembros = document.querySelector("#miembros").value;
-  let duis = document.querySelector("#duis").value;
-  let subsidio = document.querySelector("#subsidio").value;
-  let salario = document.querySelector("#salario").value;
-  let negocio = document.querySelector("#negocio").value;
-  let acepto = document.querySelector("#acepto").value;
-  let resp = document.querySelector("#respuesta");
-
-  resp.classList.remove("fail");
-  resp.classList.remove("send");
-
-  let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
-		*San José Guayabal*%0A
-		*Formulario Paquete Solidario*%0A%0A
-		*¿Cuál es tu nombre?*%0A
-		${nombre}%0A
-		*DUI:*%0A
-		${dui}%0A
-		*Dirección:*%0A
-        ${direccion}%0A
-        *Telefono:*%0A
-        ${telefonoc}%0A
-		*Miembros de la familia:*%0A
-		${miembros}%0A
-		*Duis:*%0A
-		${duis}
-		*¿Recibio Subsidio?*%0A
-		${subsidio}%0A
-		*¿Recibe Salario?*%0A
-		${salario}%0A
-		*¿Tiene negocio?*%0A
-		${negocio}%0A
-		*¿Acepto los terminos?*%0A
-		${acepto}%0A`;
-
-  if (nombre === "" || dui === "" || direccion === "" || miembros === "" || duis === "" || subsidio === "" || salario === "" || negocio === "" || acepto === "") {
-    resp.classList.add("fail");
-    resp.innerHTML = `Faltan algunos datos, ${nombre}`;
-    return false;
-  }
-  resp.classList.remove("fail");
-  resp.classList.add("send");
-  resp.innerHTML = `Se ha enviado tu formulario, ${nombre}`;
-
-  window.open(url);
-});
+  e.preventDefault()
+  submitProduct()
+})
+  
+  var firebaseConfig = {
+    apiKey: "AIzaSyBpNdAC-sRZ8bfL7YGjuwLiK_63wI_AqAY",
+    authDomain: "formulariobd-2edb9.firebaseapp.com",
+    databaseURL: "https://formulariobd-2edb9.firebaseio.com",
+    projectId: "formulariobd-2edb9",
+    storageBucket: "formulariobd-2edb9.appspot.com",
+    messagingSenderId: "853635583965",
+    appId: "1:853635583965:web:8bb024b429ec865b17f034",
+    measurementId: "G-1VH7PCYE2E"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig)
+  
+  let database = firebase.database()
+  let ref = database.ref("products")
+  function submitProduct(){
+  
+    let data = {
+  
+  nombre : document.querySelector("#nombre").value ,
+  dui : document.querySelector("#dui").value ,
+  direccion : document.querySelector("#direccion").value ,
+  tele : document.querySelector("#tele").value ,
+  miembros : document.querySelector("#miembros").value ,
+  duis : document.querySelector("#duis").value ,
+  subsidio : document.querySelector("#subsidio").value ,
+  salario : document.querySelector("#salario").value ,
+  negocio : document.querySelector("#negocio").value ,
+  acepto : document.querySelector("#acepto").value ,
+  resp : document.querySelector("#respuesta") ,
+    }
+    ref.push(data)
+    }
